d1092d76e43a418813ab8a9447598398355b7f8f
BJT55 <1976573981@qq.com>
2021-02-24 22:19:14 +0800

    practice

---
+++ LeetCode/20210224/src/test455.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class test455 {
+
+}
+
+class Solution455 {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int numOfChildren = g.length, numOfCookies = s.length;
+        int count = 0;
+        for (int i = 0, j = 0; i < numOfChildren && j < numOfCookies; i++, j++) {
+            while (j < numOfCookies && g[i] > s[j]) {
+                j++;
+            }
+            if (j < numOfCookies) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
