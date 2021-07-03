238b78dedff77c4e45f9d8fcd454e7252af3b456
BJT55 <75152580+BJT55@users.noreply.github.com>
2021-07-03 23:47:22 +0800

    practice

---
+++ LeetCode/20210703LC/src/HJ102.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class HJ102 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        while ((str = in.readLine()) != null){
+
+        }
+    }
+}
