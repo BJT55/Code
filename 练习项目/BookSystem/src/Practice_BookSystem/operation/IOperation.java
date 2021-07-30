26fe4ad07801bf7a317a88455416b13f76433dc4
BJT55 <75152580+BJT55@users.noreply.github.com>
2021-07-30 19:59:58 +0800

    delete

--- 练习项目/BookSystem/src/Practice_BookSystem/operation/IOperation.java
+++
@@ -1,7 +0,0 @@
-package Practice_BookSystem.operation;
-
-import Practice_BookSystem.BookList;
-
-public interface IOperation {
-    void work(BookList bookList);
-}
--- 练习项目/HouseSystem/src/system/function/IFunction.java
+++
@@ -1,7 +0,0 @@
-package system.function;
-
-import system.HouseList;
-// 接口：存放 操作方法
-public interface IFunction {
-    void operate(HouseList houseList);
-}
